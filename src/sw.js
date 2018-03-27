3703d72e08f259e6775cc760976b398a67d60cbf
IIYAMA12 <jorik@253.146.028.145.hva.nl>
2018-03-27 11:21:59 +0200

    add to cache

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,20 @@
-self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
+// self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
 
-self.addEventListener('fetch', event => {
-    //event.respondWith(new Response('hijacked directly!'));
-});
+// self.addEventListener('fetch', event => {
+//     //event.respondWith(new Response('hijacked directly!'));
+// });
+
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open("first").then(function(cache) {
+        return cache.add(
+            '/offline'
+        );
+        // return cache.addAll(
+        //     ['/offline']
+        // );
+      }).then(self.skipWaiting())
+    );
+  });
+  
